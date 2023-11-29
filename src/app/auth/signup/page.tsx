a4e0055fca82056519084a12787803c749998453
Dan <42155753+ochoadan@users.noreply.github.com>
2023-11-29 16:15:15 -0500

    11/29/2023

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,130 +1,46 @@
-const Page = () => {
+import Link from "next/link";
+import { Logo } from "@/components/Logo";
+import { auth } from "auth";
+import { redirect } from "next/navigation";
+import { Metadata } from "next";
+import SignUpAuthUserForm from "@/components/auth/SignUpAuthUserForm";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up for an account.",
+};
+
+export default async function Example() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
-    <div>
-      Enter
-    </div>
+    <>
+      <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <Logo className="mx-auto h-12 w-auto text-sky-600" />
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Create a new account
+          </h2>
+        </div>
+
+        <SignUpAuthUserForm />
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already a member?{" "}
+            <Link
+              href="/auth/login"
+              className="font-semibold leading-6 text-sky-600 hover:text-sky-500"
+            >
+              Log In
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default Page;
-
-// import { FaGoogle, FaMicrosoft } from "react-icons/fa6";
-// import Link from "next/link";
-// import { Logo } from "@/components/Logo";
-
-// export default function Example() {
-//   return (
-//     <>
-//       <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-//         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-//           <Logo className="mx-auto h-12 w-auto text-sky-600" />
-//           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-//             Sign in to your account
-//           </h2>
-//         </div>
-
-//         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-//           <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-//             <form className="space-y-6" action="#" method="POST">
-//               <div>
-//                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-//                   Email address
-//                 </label>
-//                 <div className="mt-2">
-//                   <input
-//                     id="email"
-//                     name="email"
-//                     type="email"
-//                     autoComplete="email"
-//                     required
-//                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-//                   />
-//                 </div>
-//               </div>
-
-//               <div>
-//                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-//                   Password
-//                 </label>
-//                 <div className="mt-2">
-//                   <input
-//                     id="password"
-//                     name="password"
-//                     type="password"
-//                     autoComplete="current-password"
-//                     required
-//                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-//                   />
-//                 </div>
-//               </div>
-
-//               <div className="flex items-center justify-between">
-//                 <div className="flex items-center">
-//                   <input
-//                     id="remember-me"
-//                     name="remember-me"
-//                     type="checkbox"
-//                     className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
-//                   />
-//                   <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
-//                     Remember me
-//                   </label>
-//                 </div>
-
-//                 <div className="text-sm leading-6">
-//                   <Link href="#" className="font-semibold text-sky-600 hover:text-sky-500">
-//                     Forgot password?
-//                   </Link>
-//                 </div>
-//               </div>
-
-//               <div>
-//                 <button
-//                   type="submit"
-//                   className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-//                 >
-//                   Sign in
-//                 </button>
-//               </div>
-//             </form>
-
-//             <div>
-//               <div className="relative mt-10">
-//                 <div className="absolute inset-0 flex items-center" aria-hidden="true">
-//                   <div className="w-full border-t border-gray-200" />
-//                 </div>
-//                 <div className="relative flex justify-center text-sm font-medium leading-6">
-//                   <span className="bg-white px-6 text-gray-900">Or continue with</span>
-//                 </div>
-//               </div>
-
-//               <div className="mt-6 grid grid-cols-2 gap-4">
-//                 <Link
-//                   href="#"
-//                   className="flex w-full items-center justify-center gap-3 rounded-md bg-[#333333] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
-//                 >
-//                   <FaMicrosoft className="h-5 w-5" />
-//                   <span className="text-sm font-semibold leading-6">Microsoft</span>
-//                 </Link>
-
-//                 <Link
-//                   href="#"
-//                   className="flex w-full items-center justify-center gap-3 rounded-md bg-[#333333] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
-//                 >
-//                   <FaGoogle className="h-5 w-5" />
-//                   <span className="text-sm font-semibold leading-6">Google</span>
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//           <p className="mt-10 text-center text-sm text-gray-500">
-//             Already a member?{' '}
-//             <Link href="/auth/login" className="font-semibold leading-6 text-sky-600 hover:text-sky-500">
-//               Log In
-//             </Link>
-//           </p>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -1,25 +1,37 @@
-import { auth, signIn } from "auth"
-// import { signIn, signOut, useSession } from "next-auth/react";
+import { auth, signOut } from "auth";
+import Link from "next/link";
 
-const SigninButton = async () => {
-  const session = await auth()
-  //  const { data: session } = useSession();
+export default async function SigninButton() {
+  const session = await auth();
   if (session && session.user) {
     return (
-      <div className=" flex gap-4 ml-auto">
-        <p className=" text-sky-600">{session.user.name}</p>
-        {/* <button className=" text-red-600" onClick={() => signOut()}>
-          Sign Out
-        </button> */}
+      <div className="flex flex-1 items-center justify-end gap-x-6">
+        <p className="text-gray-900">{session.user.name}</p>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button className="text-red-600">Sign Out</button>
+        </form>
       </div>
     );
   }
   return (
-    // <button className=" text-sky-600 ml-auto">
-    <button className=" text-sky-600 ml-auto" onClick={() => signIn()}>
-      Sign In
-    </button>
+    <div className="flex flex-1 items-center justify-end gap-x-6">
+      <Link
+        href="/auth/login"
+        className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
+      >
+        Log In
+      </Link>
+      <Link
+        href="/auth/signup"
+        className="rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+      >
+        Sign Up
+      </Link>
+    </div>
   );
-};
-
-export default SigninButton;
+}
