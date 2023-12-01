817c309e5bd4e1deaee809e4f7c905f7dd8ed367
Dan <42155753+ochoadan@users.noreply.github.com>
2023-12-01 11:17:47 -0500

    Update hero.tsx

--- src/components/static/hero.tsx
+++ src/components/static/hero.tsx
@@ -1,7 +1,39 @@
+"use client";
+
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import Image from "next/image";
-
+import { useState } from "react";
+import { Combobox } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/20/solid";
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+const domains = [
+  { id: 1, name: "nibarger.com", url: "nibarger.com" },
+  { id: 2, name: "witner.com", url: "witner.com" },
+  { id: 3, name: "brigidi.com", url: "brigidi.com" },
+  { id: 4, name: "ilges.com", url: "ilges.com" },
+  { id: 5, name: "rudicil.com", url: "rudicil.com" },
+  { id: 6, name: "ellifritz.com", url: "ellifritz.com" },
+  { id: 7, name: "englett.com", url: "englett.com" },
+  { id: 8, name: "maddry.com", url: "maddry.com" },
+  { id: 9, name: "padgette.com", url: "padgette.com" },
+  { id: 10, name: "barklow.com", url: "barklow.com" },
+];
 export default function Hero() {
+  const [selected, setSelected] = useState(domains[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? domains
+      : domains.filter((domain) =>
+          domain.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-8xl md:px-6 lg:px-8">
@@ -12,8 +44,8 @@ export default function Hero() {
                 Business email for individuals
               </h1>
 
-              <div className="flex gap-2 mt-6 max-w-sm lg:mx-0 mx-auto">
-                <div className="relative">
+              <div className="flex gap-2 mt-6 lg:mx-0 mx-auto">
+                <div className="relative ">
                   <label
                     htmlFor="name"
                     className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
@@ -28,25 +60,75 @@ export default function Hero() {
                     placeholder="Jane"
                   />
                 </div>
-                <div className="flex items-center font-bold text-xl">
-                  @
-                </div>
-                <div className="relative">
-                  <label
-                    htmlFor="name"
-                    className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
-                  >
-                    Last
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                    placeholder="Smith"
-                  />
-                </div>
-                <div className="flex items-center font-bold text-xl">.com</div>
+                <div className="flex items-center font-bold text-xl">@</div>
+                <Combobox value={selected} onChange={setSelected}>
+                  <div className="relative">
+                    <div className="relative w-full cursor-default rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-sky-300 sm:text-sm">
+                      <Combobox.Label
+                        htmlFor="name"
+                        className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+                      >
+                        Last
+                      </Combobox.Label>
+                      <Combobox.Input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                        placeholder="Smith"
+                        onChange={(event) => setQuery(event.target.value)}
+                        displayValue={(person: { name: string }) =>
+                          (person as { name: string }).name
+                        }
+                      />
+                    </div>
+                    <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                      {filteredPeople.length === 0 && query !== "" ? (
+                        <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                          Nothing found.
+                        </div>
+                      ) : (
+                        filteredPeople.map((domain) => (
+                          <Combobox.Option
+                            key={domain.id}
+                            className={({ active }) =>
+                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                active
+                                  ? "bg-sky-600 text-white"
+                                  : "text-gray-900"
+                              }`
+                            }
+                            value={domain}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  {domain.name}
+                                </span>
+                                {selected ? (
+                                  <span
+                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                      active ? "text-white" : "text-sky-600"
+                                    }`}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Combobox.Option>
+                        ))
+                      )}
+                    </Combobox.Options>
+                  </div>
+                </Combobox>
               </div>
             </div>
           </div>
