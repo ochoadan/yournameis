29272945375a84b3de529426ddd19d51f03a6cf5
Dan <42155753+ochoadan@users.noreply.github.com>
2024-02-02 10:44:09 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: "/og-image.jpg",
+        url: "https://yourname.is/og-image.jpg",
         width: 1200,
         height: 628,
         alt: "Og Image Alt"
