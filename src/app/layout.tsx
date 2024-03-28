bfcc85c7c99777e43bd4a866555817f822d06256
Dan <diogamb@gmail.com>
2024-03-27 20:16:06 -0400

    FontUpdate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Mulish } from "next/font/google";
+import { Wix_Madefor_Display } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Mulish({ subsets: ["latin"] });
+const sourceFont = Wix_Madefor_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "YourNameIs",
@@ -30,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={inter.className}>
+      <body className={sourceFont.className}>
         {/* <Banner /> */}
         {/* <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8"> */}
         {children}
