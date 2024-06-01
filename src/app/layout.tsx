df547e1942bd60090059fcc1dcff74e39e716365
Your Name <diogamb@gmail.com>
2024-05-31 22:27:33 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Wix_Madefor_Display } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Script from "next/script";
 
 const sourceFont = Wix_Madefor_Display({ subsets: ["latin"] });
 
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
   title: "YourNameIs",
   description: "YourNameIs provides business email for individuals.",
   twitter: {
-    creator: "@halfnine"
+    creator: "@halfnine",
   },
   openGraph: {
     images: [
@@ -17,11 +18,25 @@ export const metadata: Metadata = {
         url: "https://yourname.is/og-image.jpg",
         width: 1200,
         height: 628,
-        alt: "Og Image Alt"
-      }
-    ]
-  }
+        alt: "Og Image Alt",
+      },
+    ],
+  },
 };
+
+function Clarity() {
+  return (
+    <Script id="clarity-script">
+      {`
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "mknls3wxr9");
+      `}
+    </Script>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -31,6 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body className={sourceFont.className}>
+        <Clarity />
         {/* <Banner /> */}
         {/* <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8"> */}
         {children}
@@ -51,7 +67,7 @@ function Banner() {
           className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#80e6ff] to-[#688afd] opacity-30"
           style={{
             clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
         />
       </div>
@@ -63,13 +79,17 @@ function Banner() {
           className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#80e6ff] to-[#688afd] opacity-30"
           style={{
             clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
         />
       </div>
       <p className="text-sm leading-6 text-gray-900">
-        We are Migrating and Upgrading our Application, To create an email route{' '}
-        <Link href="https://www.halfnine.com/contact" target="_blank" className="whitespace-nowrap font-semibold">
+        We are Migrating and Upgrading our Application, To create an email route{" "}
+        <Link
+          href="https://www.halfnine.com/contact"
+          target="_blank"
+          className="whitespace-nowrap font-semibold"
+        >
           Contact Support &nbsp;<span aria-hidden="true">&rarr;</span>
         </Link>
       </p>
@@ -80,5 +100,5 @@ function Banner() {
         </button>
       </div> */}
     </div>
-  )
+  );
 }
